2a21cddf5a64f0245848ce1bc60eab14e9c19e60
MoonLGH <MoonLMC1212@gmail.com>
2023-02-15 02:55:07 +0000

    use first opt

--- extensions/nekopoi.ts
+++ extensions/nekopoi.ts
@@ -138,7 +138,7 @@ export async function watch(id: string): Promise<EpsWatch> {
     if (element.title.includes("360")) {
       try {
         const prov = element.list.find((item) => item.provider.includes("ouo"));
-        const url = await client.Ouo(prov!.link, 2);
+        const url = await client.Ouo(prov!.link);
         console.log(url);
         const downloads = await client.bypassMirrored(url!+"");
         console.log(downloads);
